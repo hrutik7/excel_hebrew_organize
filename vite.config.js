d6ba570e277cbea3b2e855d8e59bdc2e9a0a3421
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2024-12-18 16:05:22 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        // No need to rewrite as we've added the prefix in Flask
-        // rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: isProduction ? 'https://excel-hebrew-organize.onrender.com' : 'http://localhost:5000',
+          changeOrigin: true,
+          // No need to rewrite as we've added the prefix in Flask
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
